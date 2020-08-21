548be20cec5e1179c4bb9ec51f21696fc0ae1721
hayashiman <shikamo.hayashi@gmail.com>
2020-08-21 19:38:40 +0900

    change file

--- discordbot.py
+++ discordbot.py
@@ -163,6 +163,6 @@ def get_name(message):
 async def greet(channel):
 	await channel.send('よろしくお願いします。ゲームを開始する場合\"start\"と入力してください。')
 
-if __name__ == "__main__":
-	# Botの起動とDiscordサーバーへの接続
-	client.run(TOKEN)
+
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
